cbbc49dc4cf80fa968af04ad8502cf0fea39d4c3
Viktor Deinyi <deinyiviktor@gmail.com>
2022-07-25 23:11:55 +0300

    Seven commit change Task1

--- Tasks/src/Task1_NameAdress.java
+++ Tasks/src/Task1_NameAdress.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Task1_NameAdress {
     public static void main (String [] args) {
         String name, address;
+        Scanner name_add = new Scanner(System.in);
         System.out.println("What is your name ?");
-        Scanner scs1 = new Scanner(System.in);
-        name = scs1.nextLine();
+        name = name_add.nextLine();
         System.out.println("Where are you live ?");
-        Scanner scs2 = new Scanner(System.in);
-        address = scs2.nextLine();
+        address = name_add.nextLine();
         System.out.print("So, here is your data:\n " + "Name - " + name + "| Address - " + address);
     }
 }
