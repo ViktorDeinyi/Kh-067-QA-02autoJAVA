3c3cb491ac54a17bef85033bf8640b621642e221
Viktor Deinyi <deinyiviktor@gmail.com>
2022-07-28 15:32:41 +0300

    Ten commit change Calculator 1.1

--- Tasks/src/Calculator.java
+++ Tasks/src/Calculator.java
@@ -1,35 +1,59 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 
-        public static void main (String[] args) {
-            double sum, num1, num2;
-            Scanner scd = new Scanner(System.in);
-            Scanner scs = new Scanner(System.in);
-            System.out.println("Давай попробуем что-то посчитать");
-            System.out.println("Введи первую цифру...");
-            num1 = scd.nextDouble();
-            System.out.print(num1);
-            System.out.println(" |Теперь оператор...");
-            String arg = scs.nextLine();
-            while (!arg.equals("+")&!arg.equals("-")&!arg.equals("*")&!arg.equals("/")&!arg.equals("%")) {
-                System.out.println("С таким оператором мы еще не научились работать...  \nДавай попробуем ввести другой:");
-                arg = scs.nextLine();}
-            System.out.print(num1 + " " + arg);
-            System.out.println(" |Ну а теперь вторую цифру...");
-            num2 = scd.nextDouble();
-            if (arg.equals("+")) { sum = num1 + num2;
-                System.out.println("Результат сложения: "+ num1 + " " + arg + " " + num2 + " = " + sum );}
-            else if (arg.equals("-")) { sum = (num1 - num2);
-                System.out.println("Результат вычитания: "+ num1 + " " + arg + " " + num2 + " = " + sum );}
-            else if (arg.equals("*")) { sum = (num1 * num2);
-                System.out.println("Результат умножения: "+ num1 + " " + arg + " " + num2 + " = " + sum );}
-            else if (arg.equals("/")) { sum = (num1 / num2);
-                System.out.println("Результат деления: "+ num1 + " " + arg + " " + num2 + " = " + sum );}
-            else if (arg.equals("%")) { sum = (num1 % num2);
-                System.out.println("Результат вычисления модуля: "+ num1 + " " + arg + " " + num2 + " = " + sum );}
-            else {
+    public static void main(String[] args) {
+        final String PLUS = "+", MINUS = "-", MULTIPLY = "*", DIVIDE = "/", MODULUS = "%";
+        double sum, num1, num2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Давай попробуем что-то посчитать");
+        System.out.println("Введи первую цифру...");
+        while (true) {
+            try { num1 = sc.nextDouble(); break; }
+            catch (InputMismatchException ime) {
+                System.out.println("Хм, похоже ввели некорректный формат цифр ...");
+                System.out.println("Давай попробуем еще разок:");
+                sc.nextLine();}}
+        sc.nextLine();
+        System.out.print(num1);
+        System.out.println(" |Теперь оператор...");
+        String arg = sc.nextLine();
+        while (!arg.equals(PLUS) & !arg.equals(MINUS) & !arg.equals(MULTIPLY) & !arg.equals(DIVIDE) & !arg.equals(MODULUS)) {
+            System.out.println("С таким оператором мы еще не научились работать...  \nДавай попробуем ввести другой:");
+            arg = sc.nextLine();}
+        System.out.print(num1 + " " + arg);
+        System.out.println(" |Ну а теперь вторую цифру...");
+        while (true) {
+            try { num2 = sc.nextDouble(); break; }
+            catch (InputMismatchException ime) {
+                System.out.println("Хм, похоже ввели некорректный формат цифр ...");
+                System.out.println("Давай попробуем еще разок:");
+                sc.nextLine();}}
+        switch (arg) {
+            case PLUS:
+                sum = num1 + num2;
+                System.out.println("Результат сложения: " + num1 + " " + arg + " " + num2 + " = " + sum);
+                break;
+            case MINUS:
+                sum = num1 - num2;
+                System.out.println("Результат вычитания: " + num1 + " " + arg + " " + num2 + " = " + sum);
+                break;
+            case MULTIPLY:
+                sum = (num1 * num2);
+                System.out.println("Результат умножения: " + num1 + " " + arg + " " + num2 + " = " + sum);
+                break;
+            case DIVIDE:
+                sum = (num1 / num2);
+                System.out.println("Результат деления: " + num1 + " " + arg + " " + num2 + " = " + sum);
+                break;
+            case MODULUS:
+                sum = (num1 % num2);
+                System.out.println("Результат вычисления модуля: " + num1 + " " + arg + " " + num2 + " = " + sum);
+                break;
+            default:
                 System.out.println(" |Что-то пошло не так... ");
-            }
         }
+
+    }
 }
