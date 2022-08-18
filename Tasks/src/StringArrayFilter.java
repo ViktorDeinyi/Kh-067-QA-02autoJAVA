22d4a40a557869664f1ea2db38373cd5caecac3d
Viktor Deinyi <deinyiviktor@gmail.com>
2022-08-18 12:06:50 +0300

    16 commit change Stringarrayfilter

--- Tasks/src/StringArrayFilter.java
+++ Tasks/src/StringArrayFilter.java
@@ -1,87 +1,87 @@
+import com.sun.jdi.event.ThreadStartEvent;
+
 import java.util.Scanner;
 
 public class StringArrayFilter {
 
     public static void main(String[] args) {
 
-        final String Result = "Результат вашего запроса следующий :";
-        final String OneMore = "Желаете попробовать еще раз? \n1 | да \n2 | нет";
+        final String RESULT = "The result of your request :";
+        final String NO_RESULT = "There are no relevant results for this query";
+        final String ONE_MORE = "Do you want to try one more time? \n1 | yes \n2 | no";
         Scanner sc = new Scanner(System.in);
         boolean check = true;
         byte more;
-        int totalresult;
-
-
-        System.out.println("Введите слова в формате [name1, name2, ..., nameX] ");
+        int totalResult;
+        System.out.println("Enter words in the format [name1, name2, ..., nameX] ");
         String input = sc.nextLine();
         String[] words = input.split(", ");
-
         while (check) {
-            totalresult = 0;
-            System.out.println("Какой фильтр желаете применить для данных?");
-            System.out.println("1 | Поиск слов не превышаюших заданную длинну ");
-            System.out.println("2 | Поиск слов начинающихся с заданных символов");
-            System.out.println("3 | Поиск слов заканчивающихся на заданные символы");
-            System.out.println("4 | Поиск слов содержащих в себе комбинацию символов");
-            System.out.println("0 | Выход");
+            totalResult = 0;
+            System.out.println("Which filter do you want to apply?");
+            System.out.println("1 | Search for words not exceeding a given length ");
+            System.out.println("2 | Search for words starting with given characters");
+            System.out.println("3 | Search for words ending with given characters");
+            System.out.println("4 | Search for words containing a combination of characters");
+            System.out.println("0 | Exit");
             int filter = sc.nextInt();
             switch (filter) {
                 case 1:
-                    System.out.print("Введите максимальную длинну слова = ");
+                    System.out.print("Enter the maximum word length = ");
                     int leng = sc.nextInt();
-                    System.out.println(Result);
+                    System.out.println(RESULT);
                     for (int i = 0; i < words.length; i++) {
                         if (words[i].length() <= leng) {
-                            System.out.println(++totalresult + "  " + words[i]);
+                            System.out.println(++totalResult + "  " + words[i]);
                         }
                     }
-                    if (totalresult == 0) System.out.println("По данному запросу нет релевантных результатов");
-                    System.out.println(OneMore);
+                    if (totalResult == 0) System.out.println(NO_RESULT);
+                    System.out.println(ONE_MORE);
                     more = sc.nextByte();
                     if (more == 1) check = true;
                     else if (more == 2) check = false;
                     break;
                 case 2:
-                    System.out.print("Введите начальные символы = ");
+                    System.out.print("Enter starting characters = ");
                     String first = sc.next();
-                    System.out.println(Result);
+                    System.out.println(RESULT);
                     for (int i = 0; i < words.length; i++) {
                         if (words[i].startsWith(first)) {
-                            System.out.println(++totalresult + "  " + words[i]);
+                            System.out.println(++totalResult + "  " + words[i]);
                         }
                     }
-                    if (totalresult == 0) System.out.println("По данному запросу нет релевантных результатов");
-                    System.out.println(OneMore);
+                    if (totalResult == 0) System.out.println(NO_RESULT);
+                    System.out.println(ONE_MORE);
                     more = sc.nextByte();
                     if (more == 1) check = true;
                     else if (more == 2) check = false;
                     break;
                 case 3:
-                    System.out.print("Введите окончательные символы = ");
+                    System.out.print("Enter ending characters = ");
                     String last = sc.next();
-                    System.out.println(Result);
+                    System.out.println(RESULT);
                     for (int i = 0; i < words.length; i++) {
                         if (words[i].endsWith(last)) {
-                            System.out.println(++totalresult + "  " + words[i]);
+                            System.out.println(++totalResult + "  " + words[i]);
                         }
                     }
-                    if (totalresult == 0) System.out.println("По данному запросу нет релевантных результатов");
-                    System.out.println(OneMore);
+                    if (totalResult == 0) System.out.println(NO_RESULT);
+                    System.out.println(ONE_MORE);
                     more = sc.nextByte();
                     if (more == 1) check = true;
                     else if (more == 2) check = false;
                     break;
                 case 4:
-                    System.out.print("Введите символы содержащиеся в слове = ");
+                    System.out.print("Enter the characters contained in the word = ");
                     String symbol = sc.next();
-                    System.out.println(Result);
+                    System.out.println(RESULT);
                     for (int i = 0; i < words.length; i++) {
                         if (words[i].contains(symbol)) {
-                            System.out.println(++totalresult + "  " + words[i]);
+                            System.out.println(++totalResult + "  " + words[i]);
                         }
                     }
-                    if (totalresult == 0) System.out.println("По данному запросу нет релевантных результатов");
-                    System.out.println(OneMore);
+                    if (totalResult == 0) System.out.println(NO_RESULT);
+                    System.out.println(ONE_MORE);
                     more = sc.nextByte();
                     if (more == 1) check = true;
                     else if (more == 2) check = false;
@@ -91,6 +91,6 @@ public class StringArrayFilter {
                     break;
             }
         }
-        System.out.print("Спасибо за использование программы");
+        System.out.print("Thanks for using the program");
     }
 }
